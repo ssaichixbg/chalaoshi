808d838b385b4ae1bd533cc07ed07f7ac91edba3
Simon Zhang <simon@glowing.com>
2016-09-02 16:48:40 +0800

    change setting

--- main_app/settings.py
+++ main_app/settings.py
@@ -23,38 +23,10 @@ SECRET_KEY = 'f_ra_qu7hb5@0q84n%w()m^$&iej+ccwck=l8esu7z3-8*z3tr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-if 'debug' in env:
-    if env['debug'] == '1':
-        DEBUG = True
 
 ALLOWED_HOSTS = ['chalaoshi.cn', 'www.chalaoshi.cn', 'ecs.chalaoshi.cn','localhost']
 
 # Application definition
-
-if DEBUG:
-    import memcache_toolbar.panels.pylibmc
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel', 
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    #'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    #'debug_toolbar.panels.signals.SignalsPanel',
-    #'debug_toolbar.panels.logging.LoggingPanel',
-    #'debug_toolbar.panels.redirects.RedirectsPanel',
-    'memcache_toolbar.panels.pylibmc.PylibmcPanel',
-]
-
-DEBUG_TOOLBAR_CONFIG = {"JQUERY_URL": "http://code.jquery.com/jquery-2.1.1.min.js"}
-
 INTERNAL_IPS = ['101.231.69.134',] 
 
 INSTALLED_APPS = (
@@ -69,8 +41,6 @@ INSTALLED_APPS = (
     'main_app',
     'www',
     'wechat',
-    'memcache_toolbar',
-    #'debug_toolbar', # for debug,
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,8 +50,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django-datadog.middleware.DatadogMiddleware'
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+    'django-datadog.middleware.DatadogMiddleware',
 )
 #X_FRAME_OPTIONS = 'ALLOW-FROM http://www.zjustudy.com.cn'
 ROOT_URLCONF = 'main_app.urls'
@@ -92,37 +62,26 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 # Database & Cache
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-DATABASES = None
-if 'PRODUCTION' in env:
-    DATABASES = {
-        'default': {
-            'ENGINE':   'django.db.backends.mysql',
-            'NAME':     env['mysql_chalaoshi_db'],
-            'USER':     env['mysql_chalaoshi_user'],
-            'PASSWORD': env['mysql_chalaoshi_password'],
-            'HOST':     env['mysql_host'],
-            'PORT':     env['mysql_port'],
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '/tmp/mysql.sock',
+        'NAME': 'cls',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '',
+        'OPTIONS': {
+            'charset':'utf8mb4', #add emoji support
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/tmp/mysql.sock',
-            'NAME':     'cls',
-            'USER':     'root',
-            'PASSWORD': '',
-            'PORT':     '',
-        }
-    }
-    DEBUG = True
-DATABASES['default']['OPTIONS'] = {'charset':'utf8mb4'} #add emoji support
+}
+
 
 CACHES = {
     'default': {
             'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
            'LOCATION': '127.0.0.1:11211',
-         }
+    }
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
@@ -153,7 +112,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS':[
                 os.path.join(BASE_DIR, 'templates'),
-                '/usr/local/lib/python2.7/dist-packages/debug_toolbar/templates',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -176,22 +134,29 @@ EMAIL_HOST = 'smtp.exmail.qq.com'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = 'contact@zjustudy.com.cn'
 EMAIL_HOST_PASSWORD = ''
-if 'PRODUCTION' in env:
-    EMAIL_HOST_PASSWORD = env['email_password_chalaoshi']
 #EMAIL_USE_TLS = True
 
 # Wechat Config
-WECHAT = {}
-if 'PRODUCTION' in env:
-    WECHAT['APPID'] = env['wechat_chalaoshi_appid']
-    WECHAT['SECRET'] = env['wechat_chalaoshi_secret']
-    WECHAT['TOKEN'] = env['wechat_chalaoshi_token']
+WECHAT = {
+    'APPID':'',
+    'SECRET':'',
+    'TOKEN': '',
+}
 
 # Datadog Config
 DATADOG_API_KEY = ''
 DATADOG_APP_KEY = ''
 DATADOG_APP_NAME = ''
-if 'PRODUCTION' in env:
-    DATADOG_API_KEY = env['datadog_api_key_chalaoshi'] 
-    DATADOG_APP_KEY = env['datadog_app_key_chalaoshi']
-    DATADOG_APP_NAME = env['datadog_app_name_chalaoshi']
+
+# server host name
+HOST_NAME = 'http://chalaoshi.cn'
+
+from local_settings import *
+
+ROLLBAR = {
+    'access_token': '598746bacedc45dbac9b45322e108fcb',
+    'environment': 'development' if DEBUG else 'production',
+    'root': BASE_DIR,
+}
+import rollbar
+rollbar.init(**ROLLBAR)
