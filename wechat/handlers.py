945ed93e23a95bed944a702163faf8cafd35cfb4
Simon Zhang <simon@glowing.com>
2016-09-03 21:46:54 +0800

    add search in wechat

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -6,14 +6,41 @@ import urllib,json
 import socket
 
 from django.db.models import Q
+from django.conf import settings
 
 from weilib.lib import PTItem
 from weilib.lib import text_response, pic_text_response
 
 from chat_robot import robot
+from www.models import Teacher
 
 from .models import *
 
+HOST = settings.HOST_NAME
+
+def search_handler(recv_msg):
+    def search_teacher():
+        recv_content = recv_msg.content
+        teachers = Teacher.search(recv_content)
+        items = [PTItem(t.name,t.name,'', HOST + '/teacher/%d/' % t.pk) for t in teachers]
+        return items
+
+    def search_course():
+        pass
+
+
+    result = search_teacher()
+
+    if not result:
+        result = search_course()
+
+    if not result:
+        return
+
+    headerItem = PTItem('查询结果（最多显示10个结果）', '', '', HOST + '/')
+    result.insert(0, headerItem)
+
+    return pic_text_response(recv_msg,result[:10])
 
 def robot_handler(recv_msg, *args, **kwargs):
     recv_content = recv_msg.content
--- wechat/menu_custom.py
+++ wechat/menu_custom.py
@@ -1,15 +1,24 @@
 #coding=utf-8
+
 from weilib.lib import create_menu,MButton,get_token
-def create_btns():
-    btn1 = MButton(u'\ue00e进入查老师', url='http://chalaoshi.cn/?from=mp_menu')
-    m_list = [btn1, ]
+def create_btns(hostname):
+    btn1 = MButton(u'搜索', key='SEARCH')
+
+    btn2 = MButton(u'排行榜')
+    btn2.add_button(MButton(u'热门老师', url=''))
+    btn2.add_button(MButton(u'高分榜', url=''))
+    btn2.add_button(MButton(u'低分榜', url=''))
+
+    btn3 = MButton(u'\ue00e进入查老师', url=hostname + '/?from=mp_menu')
 
     #btn1.make_click(url='http://chalaoshi.cn/?from=mp_menu')
     #btn1.add_button(MButton('发Offer', url='http://weixin2py.gg-workshop.com/recent/'))
     #btn1.add_button(MButton('成绩速查', key='GPA_BRIEF'))
+
+    m_list = [btn1, btn2, btn3 ]
     return m_list
 
-def post_menu(appid, appsecret):
-    mlist = create_btns
+def post_menu(appid, appsecret, hostname):
+    mlist = create_btns(hostname)
     token = get_token(appid, appsecret)
     return create_menu(token, mlist)
