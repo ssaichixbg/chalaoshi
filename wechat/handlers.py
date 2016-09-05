c5db59a34ef150f58d1ddfc72206a77abd9f0968
Simon Zhang <simon@glowing.com>
2016-09-05 15:29:41 +0800

    fix encoding add search course

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -2,7 +2,9 @@
 # coding:utf-8
 # tuwei/handlers.py - router handlers for tuwei
 # ver 0.1 by winkidney 2014.05.10
-import urllib,json
+import urllib
+import json
+import urllib2
 import socket
 
 from django.db.models import Q
@@ -10,34 +12,50 @@ from django.conf import settings
 
 from weilib.lib import PTItem
 from weilib.lib import text_response, pic_text_response
-
 from chat_robot import robot
 from www.models import Teacher
 
 from .models import *
 
+
 HOST = settings.HOST_NAME
 
 def search_handler(recv_msg):
     def search_teacher():
-        def readable_result(item):
-            if isinstance(item, Teacher):
-                return '%8s评分:%.1f' % (t.name.decode('utf-8'), t.rate)
-
-            return str(item)
-
         recv_content = recv_msg.content
         teachers = Teacher.search(recv_content)
-        items = [PTItem(readable_result(t), readable_result(t), '', HOST + '/teacher/%d/' % t.pk) for t in teachers]
+        items = [PTItem('{t.name:<8}评分:{t.rate:.2f}'.format(t=t),
+                        '{t.name:<8}评分:{t.rate:.2f}'.format(t=t),
+                        '',
+                        HOST + '/teacher/%d/' % t.pk)
+                 for t in teachers]
         return items
 
     def search_course():
-        pass
+        recv_content = recv_msg.content
+        result = urllib2.urlopen('http://zjustudy.chalaoshi.cn/_api/teacher/list?'+urllib.urlencode({
+            'course_name':recv_content
+        })).read()
+
+        if result == '':
+            return
+        courses = json.loads(result)
+
+        items = []
+        for course in courses:
+            name = '{course.name}{course.kcdm}'.format(course=course)
+            teachers = course['teachers']
+            url = 'http://chalaoshi.cn/?'
+            for teacher in teachers[:30]:
+                url += 'q=%s&' % teacher['name']
+            item = PTItem(name, name, '', url)
+            items.append(item)
+        return items
 
     result = search_teacher()
 
-    if not result:
-        result = search_course()
+    # if not result:
+    #     result = search_course()
 
     if not result:
         return
